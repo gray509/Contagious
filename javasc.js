93c4e9406539051ca8e674b6b853915888d03191
gray509 <ddorian263@gmail.com>
2024-02-24 18:40:03 -0500

    clean up the code

--- javasc.js
+++ javasc.js
@@ -1,40 +1,42 @@
-const subGridCount = document.getElementById("mainGridId").childElementCount;
-
-
-var changeColor =(elementID) =>
-{
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    document.getElementById(elementID).style.backgroundColor = "#" + randomColor;
-    return("#" + randomColor);
+const info= {
+    elementID: "",
+    position: 0,
+    color: "",
+    subGridCount : document.getElementById("mainGridId").childElementCount
 }
-
-const transition =(color, elementID) =>
-{
-    var x = parseInt(elementID) + 1;
-    const transitionStep = () =>
-    {
-        if (x > subGridCount)
-            x = 1;
-        if (x === parseInt(elementID))
-            return;
-        const y = x.toString();
-        console.log(y);
-        delayed(y,color);
-        x ++;
-        setTimeout(transitionStep,2000);
-       
-    };
-    transitionStep();
-
-}
-
-const delayed = (y,color) =>
-{
-    document.getElementById(y).style.backgroundColor = color;
-}
-
 const contageous = (elementID)=>
 {
-    var color = changeColor(elementID);
-    transition(color, elementID);
+    info.position = elementID;
+    info.elementID = elementID;
+    setColor();
+    transition();
+}
+const setColor =() =>
+{
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    info.color = "#" + randomColor;
+    document.getElementById(info.elementID).style.backgroundColor = info.color;
+    document.getElementById("title").style.color = info.color;
+    
+}
+
+const transitionStep =() =>
+{
+    if (info.position > info.subGridCount)
+         info.position = 1;
+    if (info.position === parseInt(info.elementID))
+        return;
+    const y = info.position.toString();
+    document.getElementById(y).style.backgroundColor = info.color;
+    info.position++;
+}
+
+const transition = () =>
+{
+    info.position = parseInt(info.elementID) + 1;
+    const time = 200;
+    let id = setInterval(transitionStep,time);
+    setTimeout(() => {
+        clearInterval(id);
+    }, time * 9);
 }
